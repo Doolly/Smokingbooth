87f12d2643d2d4b4fbe7746ce0f0865198fef539
kimsooyoung <tge1375@naver.com>
2016-06-25 21:28:57 +0900

    가이드에 맞춰서 일부 수정
    9:28

--- i_hackaton_you/funtions.ino
+++ i_hackaton_you/funtions.ino
@@ -1,68 +1,45 @@
-void temp_humid()
+void GetTemp()
 {
   int err;
-  int err2; // changed
+  int err2; 
   float temp, humi;
   float temp2, humi2; // changed
   if( ((err=TOP_THERMO1.read(humi, temp))==0)&& (err2=TOP_THERMO2.read(humi2, temp2) == 0) )
   {
-    lcd.backlight();
-    lcd.display();
-    lcd.print("TEMP :    ");
-    lcd.print(temp);
-    lcd.setCursor(0,1);
-    lcd.print("HUMIDITY :");
-    lcd.print(humi);
-
-    delay(2000);
+    //공기 청정 지수 계산
+    int air_condition;
+    LcdCtrl(air_condition);
+  }
+  else 
+  {
+    LcdCtrl(-1); //온도값이 들어오지 않은 에러 상황
+  }
+}
+int GetPir()
+{
+  int pir_value  = digitalRead(PIR);
+  return pir_value;
+}
+void FanCtrl(int top, int bottom) //TOP_FAN 11 BOTTOM_FAN 9
+{
+  digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
+  
+  analogWrite(TOP_FAN, top);
+  analogWrite(BOTTOM_FAN, bottom);
+}
+void LcdCtrl(int air_condition)
+{
     lcd.clear();
     lcd.backlight();
     lcd.display();
-    lcd.print("TEMP2:    ");
-    lcd.print(temp2);
-    lcd.setCursor(0,1);
-    lcd.print("HUMIDITY2:");
-    lcd.print(humi2);
-  }
-  else
-  {
-    lcd.backlight();
-    lcd.display();
-    lcd.print("ERROR NO.: ");
-    lcd.print(err);
-  }
-  delay(2000); //2초마다 Refresh
-  lcd.clear();
+    if(air_conditon < 0)
+    {
+      lcd.print("TEMPERATURE ERROR");
+    }
+    else
+    {
+      lcd.print("Air Condition :  ");
+      lcd.print(air_condition);
+    }
 }
-void motor(int motor_speed, volatile int pir_flag)
-{
-  if(pir_flag == 0) //사람 없음 풀가동
-  {
-    analogWrite(motor_pwm1, 255);
-    analogWrite(motor_pwm2, 255);
-  }
-  else if(pir_flag == 1) //사람 없음 
-  {
-  analogWrite(11, (motor_speed / 1) * 255); //온도 습도로 모터 속도를 normalize
-  }
-}
-void pir_on()
-{
-    pir_flag = 1; //volatile int pir_flag = 0(초기값은 0)
-    lcd.clear();
-    lcd.backlight();
-    lcd.display();
-    lcd.print("hello smoker");
-}
-void pir_off()
-{
-  if(pir_flag == 1)
-  {
-    lcd.clear();
-    lcd.backlight();
-    lcd.display();
-    lcd.print("bye smoker");
-    pir_flag = 0;
-  }
-}
-
--- i_hackaton_you/i_hackaton_you.ino
+++ i_hackaton_you/i_hackaton_you.ino
@@ -3,31 +3,36 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-#define TOP_THERMO 10
+#define TOP_THERMO 7
 #define BOTTOM_THERMO 4
 #define PIR 2
 #define TOP_FAN 11
-#define BOTTOM_FAN 9
+#define BOTTOM_FAN 10
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2); //lcd init
 DHT11 TOP_THERMO1(TOP_THERMO1);
 DHT11 TOP_THERMO2(TOP_THERMO2);
 
-int GetTemp();
+void GetTemp();
 int GetPir();
 void FanCtrl(int top, int bottom);
 
-volatile int pir_flag = 0;
+void LcdCtrl(int air_condition);
+int pir_how_to;
+//volatile int pir_flag = 0; 인터럽트 안씀
 
 void setup() {
-  attachInterrupt(0, GetPir, RISING);
+//  attachInterrupt(0, GetPir, RISING); 인터럽트 안씀
+  pinMode(PIR, INPUT);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
   Serial.begin(9600);
   lcd.init();
 }
 void loop() {
   int sensor = digitalRead(pir_pin);
   Serial.println(sensor); // 이 두줄은 그냥 확인용
-
-  temp_humid();
-  motor(255, flag);
+  GetTemp();
+  GetPir();
+  FanCtrl(int top, int bottom); // pir 이란 normalize 잡아야 함
 }
